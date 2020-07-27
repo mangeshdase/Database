5260b34d8c6b6826410945d6efb12d96111a3a6c
mangeshdase <dasemangesh14@gmail.cpom>
2020-07-27 09:44:27 -0400

    assienment 5,6,7,8,9
    assienment 5,6,7,8,9

---
+++ SQL_Asgn7.sql
@@ -0,0 +1,31 @@
+﻿1) Write a query that counts all orders for October 3. ;
+	
+select count(*) from orders where odate='1990-10-03';
+
+
+
+2) Write a query that counts the number of different non-NULL city
+ values in the Customers table.;
+
+	 select count(distinct(city))from customers where city is not NULL;
+
+
+
+3) Write a query that selects each customer’s smallest order;
+  	select cnum ,min(amt) from orders group by cnum;
+
+
+4) Write a query that selects the first customer, in alphabetical order, whose name begins with G.;
+  
+	select cname from customers where cname like 'G%' order by cname limit 1;
+  
+ 
+
+5) Write a query that selects the highest rating in each city.; 
+   	select city, max(rating) as 'HIGHEST RATING' from customers group  by city;
+
+  
+6) Write a query that counts the number of salespeople registering orders for each day. 
+   (If a salesperson has more than one order on a given day, he or she should be counted only once.).;
+    
+	select odate, snum, count(distinct odate) from orders group by odate,snum;
---
+++ SQL_Asgn9.sql
@@ -0,0 +1,27 @@
+﻿1) Write a query that lists each order number followed by the name of the customer who made the order;
+	
+select o.onum ,c.cname from orders o, customers c where o.cnum=c.cnum; 
+
+
+
+
+2) Write a query that gives the names of both the salesperson and the customer 
+for each order along with the order number. ;
+	select s.sname,c.cname,o.onum from orders o, customers c, salespeople s where o.cnum=c.cnum and c.SNUM=s.snum;
+
+
+
+
+3) Write a query that produces all customers serviced by salespeople with a commission above 12%. 
+Output the customer’s name, the salesperson’s name, and the salesperson’s rate of commission;
+	
+select c.cname, s.sname,s.comm from customers c, salespeople s where c.snum=s.snum having comm>0.12; 
+
+
+
+
+4) Write a query that calculates the amount of the salesperson’s commission on each order by 
+a customer with a rating above 100.;
+	
+select o.ONUM ,s.sname, (amt*0.12)as COMM from  orders o,customers c, salespeople s
+ where o.snum = s.snum and  rating >100 ;
